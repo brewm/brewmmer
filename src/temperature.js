0b8585ed9fd9520b8e9ea69d059619edbb3cce90
Ableda Péter <abledapeter@gmail.com>
2014-02-02 19:32:50 +0100

    read buffer

--- src/temperature.js
+++ src/temperature.js
@@ -7,12 +7,23 @@ readTemperature();
 
 function readTemperature(){
 	fs.readFile('/sys/bus/w1/devices/28-0000051e015b/w1_slave', function(err, buffer)
-		{
-	      if (err){
-	         console.error(err);
-	         process.exit(1);
-	      }
+	{
+		if (err){
+			console.error(err);
+			process.exit(1);
+		}
 
-	      console.log(buffer);
-	   });
+		// Read data from file (using fast node ASCII encoding).
+		var data = buffer.toString('ascii').split(" "); // Split by space
+
+		// Extract temperature from string and divide by 1000 to give celsius
+		var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+
+		// Round to one decimal place
+		temp = Math.round(temp * 10) / 10;
+
+		console.log(data);
+
+		console.log(temp);
+	});
 };
