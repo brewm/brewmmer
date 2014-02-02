200cbe5deb9dbf11bed87ad3260df4476e227fe3
Ableda Péter <abledapeter@gmail.com>
2014-02-02 21:37:07 +0100

    debuging db on pi

--- src/temperature.js
+++ src/temperature.js
@@ -52,7 +52,7 @@ function storeTemperature(record){
 }
 
 function logTemperature(interval){
-	setInterval(storeTemperature(readTemperature), interval);
+	setInterval(storeTemperature(readTemperature()), interval);
 }
 
 
