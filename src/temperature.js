5cbee10cb591a0b9bed6100ae0f35cd9ecb85aae
Ableda Péter <abledapeter@gmail.com>
2014-02-02 19:27:37 +0100

    add temperature start msg

--- src/temperature.js
+++ src/temperature.js
@@ -1,6 +1,10 @@
 // Load node modules
 var fs = require('fs');
 
+//Run
+console.log("Temperature JS started");
+readTemperature();
+
 function readTemperature(){
 	fs.readFile('/sys/bus/w1/devices/28-0000051e015b/w1_slave', function(err, buffer)
 		{
