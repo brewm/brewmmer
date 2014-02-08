76bf48885b9e0f1e4f9caa37d1c477c27dc3a14f
Ableda Péter <abledapeter@gmail.com>
2014-02-08 17:52:14 +0100

    implement temperatures plot

--- src/gui_server.js
+++ src/gui_server.js
@@ -1,14 +1,35 @@
 var http = require('http'),
     fs = require('fs');
 
+var restify = require('restify');
+		
+// Creates a JSON client
+var client = restify.createJsonClient({
+	url: 'http://brewmmer.dyndns.org:3551'
+});
+
 
 fs.readFile('./temp_plot.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(3552);
+	if (err){
+			console.error(err);
+			process.exit(1);
+	}
+//	http.createServer(function(request, response) {  
+//		response.writeHeader(200, {"Content-Type": "text/html"});  
+//		response.write(html);  
+//		response.end();  
+//	}).listen(3552);
 });
+
+
+http.createServer(function(request, response) { 
+	//client.basicAuth('$login', '$password');
+	client.get('/temperatures/20', function(err, req, res, obj) {
+		if (err){
+			console.error(err);
+			process.exit(1);
+		}
+
+		JSON.stringify(res, null, 2));
+	});
+}).listen(3552);
