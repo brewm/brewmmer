c6147bc493d198e13d296246c59e2cd8d1d05e47
Karoly Kamaras <karoly.kamaras@prezi.com>
2014-07-28 22:29:46 +0200

    new model for recipe, fixed example

---
+++ src/models/recipe.js
@@ -0,0 +1,44 @@
+/* jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var RecipeSchema = new mongoose.Schema({
+  name:           { type: String, required: true },
+  ingredients: {
+  	water: [{
+      name:       { type: String, required: true },
+      volume:     { type: Number, required: true }
+    }],
+  	malts: [{
+      name:       { type: String, required: true },
+      weight:     { type: Number, required: true }
+    }],
+  	hops: [{
+      name:       { type: String, required: true },
+      weight:     { type: Number, required: true }
+    }],
+    yeast: {
+      name:       { type: String, required: true },
+      weight:     { type: Number, required: true }
+    }
+  },
+  mash_steps: [{
+    temperature:  { type: Number, required:true },
+    interval:     { type: Number, required:true },
+  }],
+  hop_steps: [{
+    hop:          { type: String, required:true },
+    interval:     { type: Number, required:true },
+  }],
+  extras: [{
+    name:         { type: String }
+  }],
+  evaluation:     { type: String },
+});
+
+var Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = {
+  Recipe: Recipe
+};
