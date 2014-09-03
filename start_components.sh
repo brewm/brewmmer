c1e1b6d97ebd2ad3e154081630d44992997f03a1
David Lakatos <david.lakatos.hu@gmail.com>
2014-09-03 20:55:22 +0100

    Logger WD hack

--- start_components.sh
+++ start_components.sh
@@ -3,7 +3,8 @@
 # Run this script after restart 
 
 forever start ./src/rest.js 
+cd src;
 forever start ./src/logger.js
-forever start ./src/http_server.js
+#forever start ./src/http_server.js
 
 exit 0
