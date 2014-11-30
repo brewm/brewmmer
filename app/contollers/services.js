10bf37be3707363a9c4a5978d0ded08794a3a0c8
Peter Ableda <peterableda@secretsaucepartners.com>
2014-11-30 21:39:04 +0100

    Update service REST resource

--- app/contollers/services.js
+++ app/contollers/services.js
@@ -17,55 +17,56 @@ function recordEvent(service, action, res){
 
 module.exports = function(router) {
 
-  // on routes that end in /services
+  // on routes that end in /services/logger
   // ----------------------------------------------------
-  router.route('/services')
+  router.route('/services/logger')
 
-    // get all the measurements
+    // manage logger
     .put(function(req, res) {
-      var service = req.query.service;
       var action  = req.query.action;
 
-      switch(service) {
-        case 'logger':
-          switch(action) {
-            case 'start':
-              recordEvent(service, action, res);
-              Logger.start(10);
-              break;
-            case 'stop':
-              recordEvent(service, action, res);
-              Logger.stop();
-              break;
-            case 'restart':
-              recordEvent(service, action, res);
-              Logger.stop();
-              Logger.start(10);
-              break;
-            default:
-              res.json({ error: 'Invalid action for ' + service + '!' });
-          }
+      switch(action) {
+        case 'start':
+          recordEvent('logger', action, res);
+          Logger.start(10);
           break;
-        case 'masher':
-          switch(action) {
-            case 'init':
-              Masher.init(req.query.recipe_id, res.json.bind(res));
-              break;
-            case 'start':
-              Masher.start(res.json.bind(res));
-              break;
-            case 'stop':
-              Masher.stop(res.json.bind(res));
-              break;
-            case 'status':
-              Masher.status(res.json.bind(res));
-              break;
-            default:
-              res.json({ error: 'Invalid action for ' + service + '!' });
-          }
+        case 'stop':
+          recordEvent('logger', action, res);
+          Logger.stop();
+          break;
+        case 'restart':
+          recordEvent('logger', action, res);
+          Logger.stop();
+          Logger.start(10);
           break;
         default:
-          res.json({ error: 'Invalid service!' });
+          res.json({ error: 'Invalid action for ' + 'logger' + '!' });
+      }
+    });
+
+  // on routes that end in /services/masher
+  // ----------------------------------------------------
+  router.route('/services/masher')
+
+    // manage masher
+    .put(function(req, res) {
+      var action  = req.query.action;
+
+      switch(action) {
+        case 'init':
+          Masher.init(req.query.recipe_id, res.json.bind(res));
+          break;
+        case 'start':
+          Masher.start(res.json.bind(res));
+          break;
+        case 'stop':
+          Masher.stop(res.json.bind(res));
+          break;
+        case 'status':
+          Masher.status(res.json.bind(res));
+          break;
+        default:
+          res.json({ error: 'Invalid action for ' + service + '!' });
       }
     });
 };
